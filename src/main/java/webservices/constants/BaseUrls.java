8af959c26871731cbaf9c69ba9605c11e4f68afe
Iurii_Kozak <Iurii_Kozak@epam.com>
2020-01-30 10:07:04 +0200

    fixes

--- src/main/java/webservices/constants/BaseUrls.java
+++ src/main/java/webservices/constants/BaseUrls.java
@@ -1,6 +1,6 @@
 package webservices.constants;
 
 public interface BaseUrls {
-    String baseURI = "http://localhost:8080/WEB";
+    String baseURI = "http://localhost:9090/WEB";
     String membersURI = baseURI + "/members";
 }
