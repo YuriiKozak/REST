8840b8264c45ad016b170c851bee6ff8d0d688d0
Iurii_Kozak <Iurii_Kozak@epam.com>
2020-02-26 15:18:19 +0200

    fixes

--- src/main/java/mailservice/utils/JsonObject.java
+++ src/main/java/mailservice/utils/JsonObject.java
@@ -3,20 +3,8 @@ package mailservice.utils;
 import io.restassured.response.Response;
 import org.json.JSONObject;
 
-import static mailservice.constants.IBaseResponse.*;
-
 public class JsonObject {
     public static JSONObject jsonObject(Response response) {
         return new JSONObject(response.getBody().asString());
-    }
-
-    public static JSONObject jsonObjectMeta(Response response) {
-        Log.info("Getting " + META + " from Response.");
-        return (JSONObject) jsonObject(response).get(META);
-    }
-
-    public static JSONObject jsonObjectResult(Response response) {
-        Log.info("Getting " + RESULT + " from Response.");
-        return jsonObject(response).getJSONArray(RESULT).getJSONObject(0);
     }
 }
